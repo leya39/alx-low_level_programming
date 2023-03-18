2fe6ea68e2b184ca18d9dc7aeebdb496de744284
leya <leya@kali.leya>
2023-03-18 17:39:22 +0300

    0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
+
+	printf("Success of a char: %lu byte(s)\n" (unsinged long)sizepf(df));
+	printf("Success of an int: %lu byte(s)\n" (unsinged long)sizeof(a));
+	printf("Success of a long int: %lu byte(s)\n" (unsinged long)sizeof(b));
+	printf("Success of a long long: %lu byte(s)\n" (unsinged long)long(c));
+	printf("Success of a float: %lu byte(s)\n" (unsinged long)sizeof(f));
+	return (0);
+}
