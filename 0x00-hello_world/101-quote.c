1c058c56ce37fdccfd903b6cfc2fba5d61964650
leya <leya@kali.leya>
2023-03-19 09:21:55 +0300

    0x00-hello_world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
+ *
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	writ(STDOUT_FILEND, "and that piece of art is useful\" -Dora korpar,2015-10-19\n", 59);
+	write(STDOUT_FILEND, "and that piece of art is useful
+			\" -Dora korpar,2015-10-19\n", 59);
 		return (1);
 }
